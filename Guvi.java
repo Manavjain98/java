8d69edcc93f5484f28a1fac3aeb7f66331f80a33
Manavjain98 <40588163+Manavjain98@users.noreply.github.com>
2018-06-27 11:00:44 +0530

    Create Guvi.java

---
+++ Guvi.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Guvi
+{
+    public static void main(String asd[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        System.out.println(s1.length());
+    }
+}
