06f88382510229f95e2cb37f1c61c8d6aa2f7044
Manavjain98 <40588163+Manavjain98@users.noreply.github.com>
2018-08-22 11:53:04 +0530

    Update and rename Guvi6.java to Guvi3.java

--- Guvi3.java
+++ Guvi3.java
@@ -1,18 +1,24 @@
 import java.util.*;
 import java.math.*;
-class Guvi6{
+class Guvi3{
     public static void main(String asd[]){
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
-        int b=sc.nextInt();
         String l="";
         BigInteger b1;
-        for(int i=a;i<b;i++){
-            l=String.valueOf(i);
+        if(a<=1000){
+            l=String.valueOf(a);
             b1=new BigInteger(l);
             if(b1.isProbablePrime(5)){
-                System.out.print(l+" ");
+                System.out.print("yes ");
             }
+            else{
+                System.out.print("no");
+        }
+        
+        }
+        else{
+            System.out.print("no");
         }
     }
 }
