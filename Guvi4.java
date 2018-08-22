f834e0224e657c97e61c76c9760fb7f798172fdb
Manavjain98 <40588163+Manavjain98@users.noreply.github.com>
2018-08-22 11:42:19 +0530

    Update and rename Guvi1.java to Guvi4.java

---
+++ Guvi4.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Guvi4{
+    public static void main(String asd[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(a<=1000&&b<=1000){
+        for(int i=a;i<=b;i++){
+            if(i%2==0){
+                
+            }
+            else{
+                System.out.print(i+" ");
+            }
+        }
+        }
+    }
+}
