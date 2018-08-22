0cb9812a8f3736ffc94d16c4e162cc7d06629d85
Manavjain98 <40588163+Manavjain98@users.noreply.github.com>
2018-08-22 12:10:46 +0530

    Update and rename Guvi9.java to Guvi10.java

--- Guvi10.java
+++ Guvi10.java
@@ -1,13 +1,14 @@
 import java.util.*;
 import java.math.*;
-class Guvi9{
+class Guvi10{
     public static void main(String asd[]){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int sum=1;
-        for(int i=1;i<=n;i++){
-            sum=sum*i;
+        for(int i=1;i<=5;i++){
+            sum=n*i;
+            System.out.print(sum+" ") ;
         }
-           System.out.print(sum) ;
+           
     }
 }
