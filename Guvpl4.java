c98bf613274fe02a60bc3b46d2e4776978ab70e4
Manavjain98 <40588163+Manavjain98@users.noreply.github.com>
2018-08-23 12:01:50 +0530

    Update and rename Guvpl3.java to Guvpl4.java

--- Guvpl4.java
+++ Guvpl4.java
@@ -1,11 +1,11 @@
 import java.util.*;
-class Guvpl3{
+class Guvpl4{
     public static void main(String asd[]){
         Scanner sc=new Scanner(System.in);
         String s1=sc.next();
+        String b=".";
         StringBuffer sb=new StringBuffer(s1);
-        sb=sb.reverse();
-        String d=sb.toString();
-        System.out.print(d);
+        sb=sb.append(b);
+        System.out.print(sb);
      }
 }
