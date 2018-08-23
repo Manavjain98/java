5fb5eab3df81f2ac7cf1d38f947927f4566a8cf1
Manavjain98 <40588163+Manavjain98@users.noreply.github.com>
2018-08-23 11:55:08 +0530

    Update and rename Guvpl1.java to Guvpl2.java

---
+++ Guvpl2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.math.*;
+class Guvpl2{
+    public static void main(String asd[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=1;
+        for(int i=1;i<=5;i++){
+            sum=sum*i;
+        }
+                    System.out.print(sum+" ") ;
+
+           
+    }
+}
+
