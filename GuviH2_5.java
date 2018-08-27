645101ec6416aa298c6b8d126bd32be442b9ad39
Manavjain98 <40588163+Manavjain98@users.noreply.github.com>
2018-08-27 19:01:27 +0530

    Update and rename GuviH2_2.java to GuviH2_5.java

--- GuviH2_5.java
+++ GuviH2_5.java
@@ -1,14 +1,16 @@
 import java.util.*;
-class GuviH2_2{
+class GuviH2_5{
     public static void main(String asdf[]){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int n1=sc.nextInt();
         int a[]=new int[n];
         for(int i=0;i<n;i++){
             a[i]=sc.nextInt();
         }
-        Arrays.sort(a);
-        System.out.print(a[n-n1]);
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=a[i];
+        }
+        System.out.print(sum);
     }
 }
